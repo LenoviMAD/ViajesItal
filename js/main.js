a2c962acb38f2d150a36328c3efd657ef1daa46b
LenoviMAD <madmoisess@gmail.com>
2021-05-10 13:58:34 -0400

    termino de modal italviajes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ import Modal from './modules/Modal.js';
 import Timer from './timer.js';
 // console.log(location);
 // Modules init
+const modal = new Modal()
+let btnInput = document.getElementById('btnInput');
+
+btnInput.addEventListener('click', async() => {
+    // Mostramos boton de enviar
+    modal.openModal('modalInput');
+})
