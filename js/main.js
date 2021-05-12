91216f6319a0ab079aa28ac3361d2484e6cac422
LenoviMAD <madmoisess@gmail.com>
2021-05-12 14:08:22 -0400

    Popover

--- js/main.js
+++ js/main.js
@@ -1,11 +1,68 @@
-import Modal from './modules/Modal.js';
-import Timer from './timer.js';
-// console.log(location);
-// Modules init
-const modal = new Modal()
-let btnInput = document.getElementById('btnInput');
+// import Modal from './modules/Modal.js';
+// import Timer from './timer.js';
+// // console.log(location);
+// // Modules init
 
-btnInput.addEventListener('click', async() => {
-    // Mostramos boton de enviar
-    modal.openModal('modalInput');
-})
+
+// $(function () {
+//     $('[data-toggle="popover"]').popover()
+//   })
+  
+
+// $("[data-toggle=popover]").each(function (i, obj) {
+//     $(this).popover({
+//         html: true,
+//         content: function () {
+//             var id = $(this).attr("id");
+//             return $("#popover-content-" + id).html();
+//         }
+//     });
+// });
+
+
+$("[data-toggle=popover]").each(function (i, obj) {
+    $(this).popover({
+        html: true,
+        title: function () {
+            var id = $(this).attr("id");
+            return $("#popover-content-head").html();
+        },
+        
+        content: function () {
+            return '<div class="popover-content1">' + $("#popover-content-content").html() +
+                '</div><div class="popover-footer">' + $("#popover-content-footer").html() +
+                '</div>';
+        }
+    });
+});
+
+// /* Create the popover with Header Content and Footer */
+// $('.popover-markup>[data-toggle="popover"]').popover({
+//     html: true,
+//     title: function() {
+//       var id = $(this).attr("id");
+//             return $("#popover-content-head").html();
+//     },
+//     /********** 
+//       In the content method, return a class 'popover-content1' wrapping the actual 'contents', 
+//       concatenated with a class 'popover-footer' wrapping the footer.
+//     ************/
+//     content: function() {
+//         return '<div class="popover-content1">' + $("#popover-content-content").html() +
+//           '</div><div class="popover-footer">' + $("#popover-content-footer").html() +
+//           '</div>';
+//       }    
+//   });
+  
+//   /**
+//     Allow the popover to be closed by clicking anywhere outside it.
+//   **/
+//   $('body').on('click', function(e) {
+//     $('.popover-markup>[data-toggle="popover"]').each(function() {
+//       //the 'is' for buttons that trigger popups
+//       //the 'has' for icons within a button that triggers a 
+//       if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+//         $(this).popover('hide');
+//       }
+//     });
+//   });
