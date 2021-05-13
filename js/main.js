7bbc1b4c0699c9de5ba8052749878946d37904ff
LenoviMAD <madmoisess@gmail.com>
2021-05-13 14:03:53 -0400

    se cambio popover por modal

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@
 // // Modules init
 
 
+
+    // const modal = new Modal()
+    // modal.initModal()
+
+let btnCabina = document.getElementById('cabina');
+let modalCabina = document.getElementById('modalEncomienda')
+
+btnCabina.addEventListener("click", function() {
+    modalCabina.classList.add('is-visible')
+  }); 
+
+
 // $(function () {
 //     $('[data-toggle="popover"]').popover()
 //   })
@@ -20,41 +32,29 @@
 // });
 
 
-$("[data-toggle=popover]").each(function (i, obj) {
-    $(this).popover({
-        html: true,
-        title: function () {
-            var id = $(this).attr("id");
-            return $("#popover-content-head").html();
-        },
-        
-        content: function () {
-            return '<div class="popover-content1">' + $("#popover-content-content").html() +
-                '</div><div class="popover-footer">' + $("#popover-content-footer").html() +
-                '</div>';
-        }
-    });
-});
+// $("[data-toggle=popover]").each(function (i, obj) {
+//     $(this).popover({
+//         html: true,
+//         content: function () {
+//             return '<div class="popover-content1">' + $("#popover-content-content").html() +
+//                 '</div><div class="popover-footer">' + $("#popover-content-footer").html() +
+//                 '</div>';
+//         }
+//     });
+// });
 
-
-$(document).ready( function () {
-    $('example').DataTable();
-} );
-
-// /* Create the popover with Header Content and Footer */
 // $('.popover-markup>[data-toggle="popover"]').popover({
 //     html: true,
 //     title: function() {
-//       var id = $(this).attr("id");
-//             return $("#popover-content-head").html();
+//       return $(this).parent().find('.head').html();
 //     },
 //     /********** 
 //       In the content method, return a class 'popover-content1' wrapping the actual 'contents', 
 //       concatenated with a class 'popover-footer' wrapping the footer.
 //     ************/
 //     content: function() {
-//         return '<div class="popover-content1">' + $("#popover-content-content").html() +
-//           '</div><div class="popover-footer">' + $("#popover-content-footer").html() +
+//         return '<div class="popover-content1">' + $(this).parent().find('.content').html() +
+//           '</div><div class="popover-footer">' + $(this).parent().find('.footer').html() +
 //           '</div>';
 //       }    
 //   });
@@ -71,3 +71,18 @@ $(document).ready( function () {
 //       }
 //     });
 //   });
+
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
+
+
+// $(document).ready(function(){
+//     $('#myModal').on('show.bs.modal', function (evnt) {
+
+//         var valueBottom = $(evnt.relatedTarget).val();
+//         console.log(valueBottom);       
+
+//     }); 
+// });
